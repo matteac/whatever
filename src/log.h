eb31cb9cfa5f4bc6df16e29621bf00b882dfafe6
matteac <mateoeestn2@gmail.com>
2026-04-26 08:15:50 -0300

    listeners unused attrib

--- src/log.h
+++ src/log.h
@@ -1,20 +1,16 @@
 #pragma once
 #include <stdio.h>
 
-#define LOG(file, level, fmt, ...)                                             \
-  do {                                                                         \
-    fprintf(file, "[%-14s] %s:%d @%s: " fmt "\n", level, __FILE_NAME__,        \
-            __LINE__, __func__, ##__VA_ARGS__);                                \
+#define LOG(file, level, fmt, ...)                                                                                                         \
+  do {                                                                                                                                     \
+    fprintf(file, "[%-14s] %s:%d @%s: " fmt "\n", level, __FILE_NAME__, __LINE__, __func__, ##__VA_ARGS__);                                \
   } while (0)
 
 #ifndef NO_INFO
-#define LOG_INFO(fmt, ...)                                                     \
-  LOG(stdout, "\x1b[36mINFO\x1b[0m", fmt, ##__VA_ARGS__)
+#define LOG_INFO(fmt, ...) LOG(stdout, "\x1b[36mINFO\x1b[0m", fmt, ##__VA_ARGS__)
 #else
 #define LOG_INFO(fmt, ...) ((void)0)
 #endif
 
-#define LOG_WARN(fmt, ...)                                                     \
-  LOG(stderr, "\x1b[33mWARN\x1b[0m", fmt, ##__VA_ARGS__)
-#define LOG_ERR(fmt, ...)                                                      \
-  LOG(stderr, "\x1b[35mERROR\x1b[0m", fmt, ##__VA_ARGS__)
+#define LOG_WARN(fmt, ...) LOG(stderr, "\x1b[33mWARN\x1b[0m", fmt, ##__VA_ARGS__)
+#define LOG_ERR(fmt, ...) LOG(stderr, "\x1b[35mERROR\x1b[0m", fmt, ##__VA_ARGS__)
